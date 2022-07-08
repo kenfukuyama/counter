f3f96962ff2eba5ea2c247ce4264c7e34ae480e2
Ken Fukuyama <kenfukuyama0112@gmail.com>
2022-07-08 12:50:54 -0700

    added guess game

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, redirect, request, session
+import random
 
 
 app = Flask(__name__)  
@@ -6,12 +7,16 @@ app.secret_key = "hello"
 
 # session['visitedTimes'] = 0 # cant do here
 
+# answer_key = random.randint(1, 100)
+
+
 @app.route('/')          
 def index():
     if 'visitedTimes' in session:
         session['visitedTimes'] += 1
     else: 
         session['visitedTimes'] = 0
+        session["answer_key"] = random.randint(1, 100)
 
     return render_template("index.html")
 
@@ -30,6 +35,17 @@ def add_visited_times_double():
 def clear_session():
     session.clear()
     return redirect('/')
+
+@app.route('/guessSubmit', methods=['POST'])
+def guessSubmit():
+    session["numberGuessed"] = int(request.form['guessN'])
+    print(type( session["numberGuessed"]))
+    print(type( session["answer_key"]))
+
+    return redirect('/')
+
+
+
 
 if __name__=="__main__":   
     app.run(debug=True)   
